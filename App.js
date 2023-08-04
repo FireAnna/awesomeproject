8aa190ae2582e259c323268297bf70e72e91ff7d
Anna Kucherenko <kucherenko.place@icloud.com>
2023-08-04 12:12:31 +0300

    hw3

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { useState } from 'react';
-import LoginScreen from './components/LoginScreen';
-import RegistrationScreen from './components/RegistrationScreen';
+import {
+  ImageBackground,
+  Keyboard,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import LoginScreen from "./Screens/LoginScreen";
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import styles from "./styles/styles";
+import * as Font from "expo-font";
+// import { AppLoading } from "expo";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  const [page, setPage] = useState('registration');
+  const [fontsIsLoad, setfontsIsLoad] = useState(false);
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+  const customFonts = {
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  };
 
-  const changePage = data => {
-    setPage(data);
+  useEffect(() => {
+    async function loadFontsAsync() {
+      await Font.loadAsync(customFonts);
+      setfontsIsLoad(true);
+    }
+    loadFontsAsync();
+  }, []);
+  if (!fontsIsLoad) {
+    return null;
+  }
+  const keyboardHide = () => {
+    setIsKeyboardOpen(false);
+    Keyboard.dismiss();
   };
 
   return (
-    (page == 'registration' && (
-      <RegistrationScreen changePage={() => changePage('login')} />
-    )) ||
-    (page == 'login' && (
-      <LoginScreen setPage changePage={() => changePage('registration')} />
-    ))
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("./assets/img/PhotoBG.png")}
+          style={styles.bgImg}
+        >
+          <RegistrationScreen
+            keyboardHide={keyboardHide}
+            setIsKeyboardOpen={setIsKeyboardOpen}
+            isKeyboardOpen={isKeyboardOpen}
+          />
+          {/* <LoginScreen
+            keyboardHide={keyboardHide}
+            setIsKeyboardOpen={setIsKeyboardOpen}
+            isKeyboardOpen={isKeyboardOpen}
+          /> */}
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
